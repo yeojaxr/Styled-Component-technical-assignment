731657fbcb11eaad7ce754b177199f24cae65ccc
yeojaxr <ikenurulfatihah23@gmail.com>
2021-11-01 21:48:43 +0700

    done soal 1

---
+++ soal-01/src/components/styles/Card.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+import React from "react";
+
+export const CardContainer = styled.div`
+  border: 1px solid rgba(192, 192, 192, 0.5);
+  border-radius: 0.5em;
+  width: ${(props) => (props.width !== null ? props.width : "100%")};
+  -webkit-box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.25);
+  -moz-box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.25);
+`;
+
+export const CardImage = styled.img`
+  width: 100%;
+  object-fit: cover;
+  height: 500px;
+  border-top-left-radius: 0.5em;
+  border-top-right-radius: 0.5em;
+`;
+
+export const CardBody = styled.div`
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CardPrice = styled.h5`
+  font-family: sans-serif;
+  font-size: 1.25em;
+  font-weight: 800;
+  margin: 0;
+  margin-bottom: 0.5em;
+`;
+
+export const CardProductName = styled.h6`
+  font-family: sans-serif;
+  font-size: 1.25em;
+  font-weight: 600;
+  margin: 0;
+  margin-bottom: 1em;
+  color: gray;
+`;
